9c1146d63616b01be3d123d099edf37d3eb277ad
LoneSurvivor1993 <tanzebin@foxmail.com>
2019-02-27 21:12:56 +0800

    配置文件

--- src/main/scala/conf/AppConf.scala
+++ src/main/scala/conf/AppConf.scala
@@ -1,5 +1,6 @@
 package conf
 
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -7,7 +8,10 @@ trait AppConf {
   val localURl = "local[2]"
   val clusterMasterURL = "spark://node1:7077"
   val conf = new SparkConf().setMaster(clusterMasterURL)
+
   val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+
   val hc = new HiveContext(sc)
 
 }
