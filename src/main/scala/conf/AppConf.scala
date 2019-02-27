693d516dfa965c97365bec5bd506557ff6add317
LoneSurvivor1993 <tanzebin@foxmail.com>
2019-02-27 15:52:15 +0800

    配置接口

---
+++ src/main/scala/conf/AppConf.scala
@@ -0,0 +1,13 @@
+package conf
+
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+trait AppConf {
+  val localURl = "local[2]"
+  val clusterMasterURL = "spark://node1:7077"
+  val conf = new SparkConf().setMaster(clusterMasterURL)
+  val sc = new SparkContext(conf)
+  val hc = new HiveContext(sc)
+
+}
